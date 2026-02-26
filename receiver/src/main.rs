baa64706ee5418c9098421bd3ac55811f18a86b0
0xEgao <polarb587@gmail.com>
2026-02-26 23:19:26 +0530

    receiver: lint fix

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -1,6 +1,5 @@
+// Fetch Offer from the Market
+// Just subscribe to the Queue and get the data
 fn main() {
     println!("Hello, world!");
 }
-
-// Fetch Offer from the Market
-// Just subscribe to the Queue and get the data
