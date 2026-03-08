88f9c2abb19fcf4892c6a94de2314fc73e8b7024
0xEgao <polarb587@gmail.com>
2026-03-08 18:39:32 +0530

    sender: few nits

---
+++ sender/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod config;
+pub mod dtos;
+pub mod market;
+pub mod metadata;
+pub mod routes;
+pub mod state;
--- sender/src/main.rs
+++ sender/src/main.rs
@@ -2,13 +2,6 @@ use std::net::SocketAddr;
 
 use tokio::net::TcpListener;
 
-mod config;
-mod dtos;
-mod market;
-mod metadata;
-mod routes;
-mod state;
-
 #[tokio::main]
 async fn main() {
     if let Err(err) = run().await {
@@ -17,10 +10,10 @@ async fn main() {
 }
 
 async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let config = config::Config::from_env()?;
+    let config = sender::config::Config::from_env()?;
     let addr: SocketAddr = config.bind_addr;
-    let state = state::AppState::new(config);
-    let app = routes::app(state);
+    let state = sender::state::AppState::new(config);
+    let app = sender::routes::app(state);
 
     let listener = TcpListener::bind(addr).await?;
     println!("Sender running on http://{}", addr);
